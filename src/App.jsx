e07fd2f7cee3c847176c764c5b2ea475db2c1e6c
Abduboriy <asqarovabduboriy8@gmail.com>
2024-05-09 13:03:18 +0500

    up date main

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import BtnPlus from "./components/BtnPlus/BtnPlus";
 import Btn from "./components/Btnminus/Btn";
 import Count from "./components/Count/Count";
-import Main from "./components/Main/Main";
 import Navbar from "./components/Navbar/Navbar";
 import Rest from "./components/Rest/Rest";
 
